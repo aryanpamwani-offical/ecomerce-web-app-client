166e5da099b80e16c88fd1d1f24ea6243bd0a0df
aryanpamwani-offical <113785853+aryanpamwani-offical@users.noreply.github.com>
2024-05-07 14:58:37 +0530

    Ready For Deployment

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.css';
  import { useEffect } from 'react';
 import Navbar from "../component/Navbar/navbar";
-
+import Head from 'next/head'
 
 import { Provider } from "react-redux";
 import store from "@/redux/store";
@@ -27,7 +27,12 @@ export default function RootLayout({ children }) {
      
 
 <Provider store={store}>
-
+<Head>
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+<link rel="manifest" href="/site.webmanifest"></link>  
+    </Head>
 
         <Navbar/>
         {children}
